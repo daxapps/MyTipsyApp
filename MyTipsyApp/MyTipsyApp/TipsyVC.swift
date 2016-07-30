91260e0b51e3fb6b7df350b40efa206ab7100f18
Jason Crawford <daxapps777@gmail.com>
2016-07-30 11:02:15 -0500

    finished layout

---
+++ MyTipsyApp/MyTipsyApp/TipsyVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyTipsyApp
+//
+//  Created by Jason Crawford on 7/30/16.
+//  Copyright © 2016 Dax Apps. All rights reserved.
+//
+
+import UIKit
+
+class TipsyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+   
+
+
+}
+
